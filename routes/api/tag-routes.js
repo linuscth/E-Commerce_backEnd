6d8d38074ea51d896fa3d40f7a0d481231bcf660
Ting hon Linus cheung <linuscheung@Tings-MBP-2.home>
2023-07-05 23:49:13 -0700

    first commit

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,87 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  try {
+    // find all tags
+    // be sure to include its associated Product data
+    const allTagData = await Tag.findAll({
+      include: [{
+        model: Product, through: ProductTag, as: 'products'
+      }]
+    })
+    res.status(200).json(allTagData)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
+router.get('/:id', async (req, res) => {
+
+  try {
+    // find a single tag by its `id`
+    // be sure to include its associated Product data
+    const selectedTagIddata = await Tag.findByPk(req.params.id, {
+      include: [{
+        model: Product, through: ProductTag, as: 'products'
+      }]
+    })
+    res.status(200).json(selectedTagIddata)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
+
+router.post('/', async (req, res) => {
+  // create a new tag
+  try {
+    const newTagName = req.body;
+    // const checkTagName = await Tag.findOne({ where: { newTagName } });
+    // if (checkTagName) {
+    //   res.status(404).json({ message: 'tag already exist' })
+    // }
+    const createNewTagName = await Tag.create(newTagName);
+    res.status(200).json(createNewTagName);
+
+  } catch (error) {
+    res.status(404).json(error)
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a tag's name by its `id` value
+  try {
+    const tagId = req.params.id;
+    const updatedTagId = await Tag.update(req.body,
+      {
+        where: {
+          id: tagId
+        }
+      })
+    res.status(200).json({ message: 'update succeeded' })
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
+
+router.delete('/:id', async (req, res) => {
+  // delete on tag by its `id` value
+  try {
+    const targetId = req.params.id;
+    const destroyId = await Tag.destroy({
+      where: { id: targetId }
+    })
+    if (!destroyId) {
+      res.status(404).json({ message: 'No Tag found with this id!' })
+    }
+    res.status(200).json(destroyId);
+  } catch (error) {
+    res.status(500).json(error)
+  }
+});
+
+module.exports = router;
